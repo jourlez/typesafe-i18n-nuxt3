8baad8b85437df269ee58c78140025c5bf6a0393
jourlez <josuecr.288@gmail.com>
2022-07-21 13:34:50 -0600

    plugin optimization

--- plugins/i18n.ts
+++ plugins/i18n.ts
@@ -1,8 +1,9 @@
-import { i18nPlugin } from '~~/i18n/i18n-vue'
-import { defineNuxtPlugin } from '#app'
-import { loadAllLocales } from '~~/i18n/i18n-util.sync'
+import { i18nPlugin } from '../i18n/i18n-vue';
+import { defineNuxtPlugin } from '#app';
+import { loadLocaleAsync } from '../i18n/i18n-util.async';
 
-export default defineNuxtPlugin(nuxtApp => {
-   loadAllLocales()
-   nuxtApp.vueApp.use(i18nPlugin, 'en')
-})
+export default defineNuxtPlugin(async (nuxtApp) => {
+  const locale = 'en'; // TODO: detect locale
+  await loadLocaleAsync(locale);
+  nuxtApp.vueApp.use(i18nPlugin, locale);
+});
